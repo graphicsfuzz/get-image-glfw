4420f8ebf58e1ec06453a16e3e1aee8b5ea1eb05
Paul Thomson <paul.thomson11@imperial.ac.uk>
2017-02-14 12:25:42 +0000

    Weird fix for automated build.

--- docker_build_create_start.sh
+++ docker_build_create_start.sh
@@ -12,5 +12,5 @@ docker stop ${CONTAINER} || true
 docker rm ${CONTAINER} || true
 
 docker create -e CI_BUILD_REF -e GITHUB_TOKEN -ti --name ${CONTAINER} ${IMAGE}
-docker start -ai ${CONTAINER}
+docker start -a ${CONTAINER} | cat
 
