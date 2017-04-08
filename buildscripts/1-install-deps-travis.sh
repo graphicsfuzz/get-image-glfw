4555493a65dd571302f966185c6c72a7109da3a9
Paul Thomson <paul.thomson11@imperial.ac.uk>
2017-04-08 13:10:56 +0100

    Small fix to travis script.

--- buildscripts/1-install-deps-travis.sh
+++ buildscripts/1-install-deps-travis.sh
@@ -9,10 +9,6 @@ sudo chmod uga+rwx /data/bin
 GITHUB_RELEASE_TOOL_USER="paulthomson"
 GITHUB_RELEASE_TOOL_VERSION="v1.0.9.1"
 
-
-mkdir deps
-cd deps
-
 if [ "$(uname)" == "Darwin" ];
 then
   GITHUB_RELEASE_TOOL_ARCH="darwin_amd64"
@@ -23,9 +19,6 @@ then
   GITHUB_RELEASE_TOOL_ARCH="linux_amd64"
   sudo apt-get -y install g++ libxrandr-dev libxinerama-dev libxcursor-dev libgl1-mesa-dev cmake zip git
 fi
-
-cd ..
-
 
 pushd /data/bin
 wget "https://github.com/${GITHUB_RELEASE_TOOL_USER}/github-release/releases/download/${GITHUB_RELEASE_TOOL_VERSION}/github-release_${GITHUB_RELEASE_TOOL_VERSION}_${GITHUB_RELEASE_TOOL_ARCH}.tar.gz"
